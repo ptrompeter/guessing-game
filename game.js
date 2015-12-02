de73cb9f4aec4857dac14b729b843410e7f58ff1
ptrompeter <patrick.trompeter@gmail.com>
2015-12-02 11:28:03 -0800

    program now has 5 questions.  User prompted for number of questions, questions randomly selected.  All functions separated.

--- game.js
+++ game.js
@@ -1,45 +1,116 @@
 
-alert("Time for a guessing game about me!");
-var name = prompt("What's my first name?");
-var tv = prompt("What tv show did I mention in my Code Fellow's self-introduction?");
-var job = prompt("What was my last job?  No articles, please - just a word.");
-var age = prompt("How old am I? (As of December 2015.)");
+//global variables
+var numOfQs;
+var qNumArray = [];
+var name;
+var tv;  
+var job;  
+var age;  
+var pet; 
 var counter = 0;
 var wrongAnswer = [];
-if (name.toLowerCase() === 'patrick') {
-  counter++;
-} else {
-  wrongAnswer.push(" My first name isn't " + name + ".");
+var checkNum;
+
+//declaring functions
+
+function qNumber(){
+  numOfQs = parseInt(prompt("How many questions do you want? Choose a number less than or equal to 5."));
+  if (!(numOfQs > 0 && numOfQs <= 5)){
+    alert("Please select an interger greater than 0 and less than 6.");
+    qNumber();
+  } 
+}
+function repeatChecker(){
+  for (var i = 0; i < qNumArray.length; i++){
+    if (checkNum === qNumArray[i]){
+      return true;
+    }
+  }
+}
+function qArrayMaker() {
+  while (qNumArray.length < numOfQs){
+    checkNum = Math.ceil(Math.random() * 5);
+      if(!repeatChecker()){
+        qNumArray.push(checkNum);
+      }
+  }
+}
+function gameFunc(array) {
+  for (i = 0; i < array.length; i++){
+    if (array[i] === 1){
+      nameFunc();
+    } else if (array[i] === 2){ 
+      tvFunc();
+    } else if (array[i] === 3){
+      jobFunc();
+    } else if (array[i] === 4){
+      ageFunc();
+    } else {
+      petFunc();
+    }
+  }
 }
 
-if (tv.toLowerCase() === 'supernatural'){
-  counter++;
-} else {
-  wrongAnswer.push(" " + tv + " wasn't the show I mentioned in class.");
+function nameFunc() {
+  name = prompt("What's my first name?");
+  if (name.toLowerCase() === 'patrick') {
+    counter++;
+  } else {
+    wrongAnswer.push(" My first name isn't " + name + ".");
+  }
 }
-if ((job.toLowerCase() === "lawyer") || (job.toLowerCase() === "attorney")) {
-  counter++;
-} else {
-  wrongAnswer.push(" I didn't work as a(n) " + job + " for my last job.");
-}
-function ageQuestion(){
 
+function tvFunc() {
+  tv = prompt("What tv show did I mention in my Code Fellow's self-introduction?");
+  if (tv.toLowerCase() === 'supernatural'){
+    counter++;
+  } else {
+    wrongAnswer.push(" " + tv + " wasn't the show I mentioned in class.");
+  }
+}
+function jobFunc() {
+  job = prompt("What was my last job?  No articles, please - just a word.");
+  if ((job.toLowerCase() === "lawyer") || (job.toLowerCase() === "attorney")) {
+    counter++;
+  } else {
+    wrongAnswer.push(" I didn't work as a(n) " + job + " for my last job.");
+  }
+}
+function ageFunc(){
+  age = prompt("How old am I? (As of December 2015.)");
   if (age === "quit" || age === "Quit" || age === "q" || age === "Q") {
       alert("You suck at writing numbers.");
   } else if (!parseInt(age)){
       age = prompt("Please enter an interger value for my age, or type 'quit'.");
-      ageQuestion();
+      ageFunc();
   } else if (parseInt(age) === 37){
       counter++;
   } else {
       wrongAnswer.push(" I'm not " + age + " years old.");
   }
 }
-ageQuestion();
-console.log(wrongAnswer);
-
-if (counter === 4) {
-  alert("You got them all right!");
-} else {
-  alert("You got " + counter + " answer(s) correct. " + wrongAnswer.join(' '));
+function petFunc() {
+  pet = prompt("What was the name of my first pet?");
+  if (pet.toLowerCase() === 'claw'){
+    counter++;
+  } else {
+    wrongAnswer.push(" my first pet wasn't named " + pet + ".");
+  }
 }
+function answers() {
+    if (counter === numOfQs) {
+    alert("You got them all right!");
+  } else {
+    alert("You got " + counter + " answer(s) correct. " + wrongAnswer.join(' '));
+  }
+}
+
+//function calls
+qNumber();
+console.log("numOfQs = " + numOfQs);
+qArrayMaker();
+console.log("qNumArray = " + qNumArray);
+gameFunc(qNumArray);
+answers();
+
+
